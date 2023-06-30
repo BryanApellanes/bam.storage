bc874cb1c66ea22112af03ffffaf968d552f58ab
Bryan Apellanes <bryan@threeheadz.com>
2023-06-29 19:53:35 -0500

    initial

---
+++ bam.storage/IAsyncStorage.cs
@@ -0,0 +1,18 @@
+﻿using Bam.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Storage
+{
+    public interface IAsyncStorage : IStorage
+    {
+        Task<T> LoadAsync<T>(string path);
+
+        Task<T[]> SearchAsync<T>(IStorageSearchFilter searchFilter);
+
+        Task<IStorageResult> SaveAsync<T>(string path, T value);
+    }
+}
---
+++ bam.storage/IStorage.cs
@@ -0,0 +1,18 @@
+﻿using Bam.Storage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bam.Storage
+{
+    public interface IStorage
+    {
+        T Load<T>(string path);
+
+        T[] Search<T>(IStorageSearchFilter searchFilter);
+
+        IStorageResult Save<T>(string path, T value);
+    }
+}
